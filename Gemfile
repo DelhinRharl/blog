83dcc9954e3a9e217d740a4579bfe534d32e64e1
DelhinRharl <delhinrharl@gmail.com>
2022-04-16 13:40:05 +0100

    Added documentation

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby '3.0.1'
+ruby '3.1.1'
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem 'rails', '~> 7.0.2', '>= 7.0.2.3'
@@ -33,7 +33,6 @@ gem 'rspec-rails'
 gem 'rswag'
 gem 'stimulus-rails'
 gem 'tzinfo-data'
-gem 'rswag'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
@@ -56,5 +55,4 @@ group :test do
   gem 'capybara'
   gem 'selenium-webdriver'
   gem 'webdrivers'
-  gem 'rswag'
 end
--- spec/requests/api/posts_spec.rb
+++ spec/requests/api/posts_spec.rb
@@ -2,9 +2,7 @@ require 'swagger_helper'
 
 RSpec.describe 'api/posts', type: :request do
   describe 'Blog API' do
-
     path '/api/v1/posts' do
-  
       post 'Creates a post' do
         tags 'posts'
         consumes 'application/json'
@@ -14,14 +12,14 @@ RSpec.describe 'api/posts', type: :request do
             title: { type: :string },
             text: { type: :string }
           },
-          required: [ 'title', 'text' ]
+          required: %w[title text]
         }
-  
+
         response '201', 'post created' do
           let(:posts) { { title: 'foo', text: 'bar' } }
           run_test!
         end
-  
+
         response '422', 'invalid request' do
           let(:posts) { { title: 'foo' } }
           run_test!
